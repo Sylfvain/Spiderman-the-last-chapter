c300c17930ae3eefca31999b55c931489986273d
Sylia <152609921+Sylfvain@users.noreply.github.com>
2023-12-05 11:14:01 +0100

    Space_Invaders:prueba
    Código inicial.

---
+++ Background.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Background here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Background extends World
+{
+
+    /**
+     * Constructor for objects of class Background.
+     * 
+     */
+    public Background()
+    
+    {
+        //Create a new scenario with 600*400 cells with a cell side of 1*1 pixels
+        super(600,400,1);
+        
+        setBackground("Pa.jpg");
+    }
+    
+    public void started()  {
+        
+        
+    }
+}
