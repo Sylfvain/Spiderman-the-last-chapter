662f3d5d1082fd12b06a96f1ea7c8bee0798ac24
Sylia <152609921+Sylfvain@users.noreply.github.com>
2023-12-11 18:28:27 +0100

    Intento de Fase 3
    No me sale el actor. :/

---
+++ -_-/Spiderman.java
@@ -0,0 +1,20 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Spiderman extends Actor
+{
+
+    /**
+     * Act - do whatever the Spiderman wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        /* /El personaje se mueve tres celdas en la dirección que tenga asignada*/
+        move(3);
+    }
+}
