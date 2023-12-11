991e470e9084a861da115100dd95e272b2b28765
Sylia <152609921+Sylfvain@users.noreply.github.com>
2023-12-11 17:44:29 +0100

    Fase 1 y 2 del proyecto
    Solo aparece el escenario.

---
+++ -_-/NuevoEscenario.java
@@ -0,0 +1,24 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class NuevoEscenario extends World
+{
+
+    /**
+     * Constructor for objects of class NuevoEscenario.
+     */
+    public NuevoEscenario()
+    {
+        /* Create a new world with 600x400 cells with a cell size of 1x1 pixels.*/
+        super(600, 400, 1);
+        setBackground("Captura de pantalla 2023-12-05 163710.png");
+        
+        
+        
+    }
+}
