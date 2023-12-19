4946988b9e2dbc991cecc8a030dc4cf1e3eb2812
WonShuHong <152609863+WonShuHong@users.noreply.github.com>
2023-12-19 11:14:15 +0100

    Intento 4 fase 4

---
+++ El de verdad 2.0/Spiderman.java
@@ -0,0 +1,47 @@
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Spiderman extends Actor
+{
+
+    /**
+     * 
+     */
+    public Spiderman()
+    {
+        setImage ( "Spidey.png" );
+    }
+
+    /**
+     * Act - do whatever the Spiderman wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        /* /El personaje se mueve tres celdas en la dirección que tenga asignada*/
+        if (Greenfoot . isKeyDown ( "Right" )) {
+            if (! isAtEdge ( )) {
+                move ( 5 );
+            }
+        }
+        if (Greenfoot . isKeyDown ( "Left" )) {
+            if (! isAtEdge ( )) {
+                move ( - 5 );
+            }
+        }
+        if (isAtEdge()) {
+    
+    setRotation(getRotation() + 360);
+    move(5);
+}
+if (isAtEdge()) {
+    setLocation(getX() - 5, getY());
+}
+
+}
+
+    
+}
